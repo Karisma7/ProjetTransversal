93489adb1df36eb052a559894236cf3acfacfe72
SMokhtar-AV <sidelmoctar.jiyid@insa-lyon.fr>
2025-01-29 14:47:17 +0100

    initiation du projet

---
+++ src/main/java/com/AventixPay/Aventix/AventixApplication.java
@@ -0,0 +1,13 @@
+package com.AventixPay.Aventix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AventixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AventixApplication.class, args);
+	}
+
+}
